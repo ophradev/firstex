5e06fe8367ef694fd315ff611a1070d19f69e905
ophradev <111154798+ophradev@users.noreply.github.com>
2022-08-13 10:34:20 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { CoinMarketProvider } from '../context/context'
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
-      serverUrl='https://gqasdf4tkkiu.usemoralis.com:2053/server'
+      serverUrl='https://qy7wqme3wnfq.usemoralis.com:2053/server'
       appId='Azs4FBUDoDcAIZ8rshgylhRzN3MO6t6bwYNj11Fo'
     >
       <GunProvider>
